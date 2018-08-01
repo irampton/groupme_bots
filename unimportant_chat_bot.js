fc13c86d3fcf68da2b9277c037b6e213b9ff420b
TheTwitchyBlaze <Blazecool18@gmail.com>
2018-07-31 20:46:22 -0600

    Now update
    idc if what I want to do works or not I just want it done put a space if you have to

--- unimportant_chat_bot.js
+++ unimportant_chat_bot.js
@@ -51,6 +51,10 @@ app.post('/', function (req, res) {
             }
             //commands
             switch (txt) {
+                case "!heck":
+                    text = "!heck";
+                    go = true;
+                    break;
                 case "stap":
                     text = "Stop that right now Mister!";
                     go = true;
